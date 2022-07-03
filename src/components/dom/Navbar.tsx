73f84a5d421dd6a4eb6cb99de48d8530f0c794ae
Dale B <daleban.dev@gmail.com>
2022-07-03 16:30:03 +0800

    style: update navbar and add socials

--- src/components/dom/Navbar.tsx
+++ src/components/dom/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
   const router = useStore((s) => s.router)
 
   return (
-    <nav className='flex items-center justify-between w-full pt-10 text-white'>
+    <nav className='fixed top-0 left-0 right-0 flex items-center justify-between w-full px-20 pt-5 pb-1 text-white border-b border-b-white/20'>
       <span className='text-lg font-fog'>Dale B.</span>
       <span
         className='text-sm font-light tracking-widest cursor-pointer font-ubuntu'
