6a3bcdbfdb9c38b2c4911449f5baa9cbea88134c
Dale B <daleban.dev@gmail.com>
2022-07-05 20:47:34 +0800

    style: navbar item 3d hover animation

--- src/components/dom/Navbar.tsx
+++ src/components/dom/Navbar.tsx
@@ -1,20 +1,41 @@
 import useStore from '@/helpers/store'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-interface NavbarProps {}
-
-export const Navbar: React.FC<NavbarProps> = ({}) => {
+export const Navbar: React.FC = ({}) => {
   const router = useStore((s) => s.router)
 
+  useEffect(() => {
+    const menuItems = [...(document.querySelectorAll('.menu-item') as any)]
+    menuItems.forEach((item: any) => {
+      let word = item.children[0].children[0].innerText.split('')
+      item.children[0].innerHTML = ''
+      word.forEach((letter: string, idx: number) => {
+        item.children[0].innerHTML += `<span style="--index: ${idx};">${letter}</span>`
+      })
+
+      let cloneDiv = item.children[0].cloneNode(true)
+      cloneDiv.style.position = 'absolute'
+      cloneDiv.style.left = '0'
+      cloneDiv.style.top = '0'
+      item.appendChild(cloneDiv)
+    })
+  }, [])
+
   return (
-    <nav className='fixed top-0 left-0 right-0 flex items-center justify-between w-full px-20 pt-5 pb-1 text-white border-b border-b-white/20'>
-      <span className='text-lg font-fog'>Dale B.</span>
+    <nav className='fixed top-0 left-0 right-0 z-20 flex items-center justify-between w-full px-20 pt-5 pb-1 text-white border-b border-b-white/20'>
       <span
-        className='text-sm font-light tracking-widest cursor-pointer font-ubuntu'
-        onClick={() => router.push('/about')}
+        className='text-lg cursor-pointer font-fog'
+        onClick={() => router.push('/')}
       >
-        EXPERIENCE
+        Dale B.
       </span>
+      <a href='#' className='menu-item' onClick={() => router.push('/about')}>
+        <div>
+          <span className='text-sm font-light tracking-widest cursor-pointer menu-item-text font-ubuntu'>
+            EXPERIENCE
+          </span>
+        </div>
+      </a>
     </nav>
   )
 }
