d0808753dae3dfe36699ad4ff63b62f400ee9ace
Dale B <daleban.dev@gmail.com>
2022-07-07 17:21:38 +0800

    fix: navbar z-index issue

--- src/components/dom/Navbar.tsx
+++ src/components/dom/Navbar.tsx
@@ -1,14 +1,17 @@
 import useStore from '@/helpers/store'
 import React from 'react'
 
-interface NavbarProps {}
-
-export const Navbar: React.FC<NavbarProps> = ({}) => {
+export const Navbar: React.FC = ({}) => {
   const router = useStore((s) => s.router)
 
   return (
-    <nav className='fixed top-0 left-0 right-0 flex items-center justify-between w-full px-20 pt-5 pb-1 text-white border-b border-b-white/20'>
-      <span className='text-lg font-fog'>Dale B.</span>
+    <nav className='fixed top-0 left-0 right-0 z-20 flex items-center justify-between w-full px-20 pt-5 pb-1 text-white border-b border-b-white/20'>
+      <span
+        className='text-lg cursor-pointer font-fog mix-blend-difference'
+        onClick={() => router.push('/')}
+      >
+        Dale B.
+      </span>
       <span
         className='text-sm font-light tracking-widest cursor-pointer font-ubuntu'
         onClick={() => router.push('/about')}
