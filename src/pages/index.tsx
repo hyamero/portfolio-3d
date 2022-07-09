7957c46b6be0f4c9fbd9e9f41fa01fed0bde6a4e
Dale B <daleban.dev@gmail.com>
2022-07-09 18:18:23 +0800

    feat: initial landing loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,47 @@
+import { a, useChain, useSpring, useSpringRef } from '@react-spring/web'
 import Heading from '@/components/dom/Heading'
 import dynamic from 'next/dynamic'
+import React from 'react'
 
 const SceneIndex = dynamic(() => import('@/components/canvas/SceneIndex'), {
   ssr: false,
 })
 
 const Page = (props) => {
-  return <>{<Heading />}</>
+  const bgSpringRef = useSpringRef()
+  const bgSpring = useSpring({
+    ref: bgSpringRef,
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    config: { duration: 800 },
+    delay: 300,
+  })
+
+  const textSpringRef = useSpringRef()
+  const textSpring1 = useSpring({
+    ref: textSpringRef,
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    config: { duration: 800 },
+    delay: 1000,
+  })
+
+  useChain([textSpringRef, bgSpringRef])
+
+  return (
+    <>
+      <a.div
+        ref={bgSpringRef}
+        style={bgSpring}
+        className='absolute top-0 left-0 z-30 flex items-center justify-center w-screen h-screen text-3xl text-white bg-black font-fog'
+      >
+        <a.span ref={textSpringRef} style={textSpring1}>
+          Joseph Dale Bañares
+        </a.span>
+      </a.div>
+      <Heading />
+    </>
+  )
 }
 
 Page.r3f = (props) => (
