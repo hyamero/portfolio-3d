afa3c352bc57cc9d8e8116cd26592c484b379104
Dale B <daleban.dev@gmail.com>
2022-07-27 22:12:24 +0800

    chore: dynamic canvas & dom z-index

--- src/components/layout/dom.tsx
+++ src/components/layout/dom.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import useStore from '@/helpers/store'
 import { Loader } from '@/components/dom/Loader'
 import { Navbar } from '../dom/Navbar'
+import { useRouter } from 'next/router'
 
 const Dom = ({ children }) => {
   const ref = useRef(null)
@@ -10,15 +11,29 @@ const Dom = ({ children }) => {
   }, [])
 
   const [unmount, setUnmount] = useState<boolean>(false)
+  const router = useRouter()
+  const path = router.pathname
+
+  const [zIndex, setZIndex] = useState('')
+
+  useEffect(() => {
+    if (path !== '/') {
+      if (path === '/contact' && unmount) {
+        setZIndex('z-10')
+      } else {
+        setZIndex('z-12')
+      }
+    } else setZIndex('z-10')
+  }, [path, unmount])
 
   return (
     <>
       <Navbar />
       <div
-        className='absolute top-0 left-0 z-10 w-screen h-screen px-5 mx-auto overflow-hidden sm:px-10 lg:px-20 dom'
+        className={`${zIndex} absolute top-0 left-0 w-screen h-screen px-5 mx-auto overflow-hidden sm:px-10 lg:px-20 dom`}
         ref={ref}
       >
-        {!unmount && <Loader setUnmount={setUnmount} />}
+        {!unmount && path === '/' ? <Loader setUnmount={setUnmount} /> : null}
         {children}
       </div>
     </>
