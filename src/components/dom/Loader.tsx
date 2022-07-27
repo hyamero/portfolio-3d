dcc1fdf39fe38a1af0f9684bab8e4f65988e8eb2
Dale B <daleban.dev@gmail.com>
2022-07-27 22:13:42 +0800

    feat: add custom route loader

--- src/components/dom/Loader.tsx
+++ src/components/dom/Loader.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 interface LoaderProps {
   setUnmount: React.Dispatch<React.SetStateAction<boolean>>
+  text?: string
 }
 
 export const Loader: React.FC<LoaderProps> = ({ setUnmount }) => {
@@ -38,6 +39,44 @@ export const Loader: React.FC<LoaderProps> = ({ setUnmount }) => {
         className='relative text-3xl text-white font-fog bottom-8'
       >
         Joseph Dale Bañares
+      </a.span>
+    </a.div>
+  )
+}
+
+export const CustomLoader: React.FC<LoaderProps> = ({ setUnmount, text }) => {
+  const bgSpringRef = useSpringRef()
+  const bgSpring = useSpring({
+    ref: bgSpringRef,
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    config: { duration: 400 },
+    delay: 300,
+    onResolve: () => setUnmount(true),
+  })
+
+  const textSpringRef = useSpringRef()
+  const textSpring1 = useSpring({
+    ref: textSpringRef,
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    delay: 800,
+  })
+
+  useChain([textSpringRef, bgSpringRef])
+
+  return (
+    <a.div
+      ref={bgSpringRef}
+      style={bgSpring}
+      className='absolute top-0 left-0 flex items-center justify-center w-screen h-screen bg-black z-[19]'
+    >
+      <a.span
+        ref={textSpringRef}
+        style={textSpring1}
+        className='relative text-3xl text-white font-fog bottom-8'
+      >
+        {text}
       </a.span>
     </a.div>
   )
